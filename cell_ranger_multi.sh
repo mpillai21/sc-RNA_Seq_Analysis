36c2267aab26dde38b39258206c6d7e85ac59677
mpillai21 <144842242+mpillai21@users.noreply.github.com>
2024-02-01 16:59:05 -0500

    Code for alignment of multiplexed sc-RNA data

---
+++ cell_ranger_multi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+date
+#The directory to which the output must be saved 
+cd //multi_outs
+
+#Add the id to be allotted and the path to the configuration csv file containing the multiplexing information
+//softwares/cellranger-7.0.0/cellranger multi --id 1000 --csv //config/multi_config.csv --localmem=80 --localcores=16
+date
